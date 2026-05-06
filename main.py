779c75833bbf3eafa912668b70f97e995003c36d
DHL <studymichik@gmail.com>
2026-05-06 10:01:54 +0900

    feat: add sub mul func

---
+++ main.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+a=int(input("첫번째 숫자:"))
+b=int(input("두번째 숫자:"))
+
+print("더하기 결과:", add(a, b))
+print("빼기 결과:", sub(a, b))
+print("곱하기 결과:", mul(a, b))
